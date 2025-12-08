32b25e497d8605a82e4d8d1fefaa8e39a28d088f
lssv6 <lucassousasilva@yahoo.com>
2025-12-08 13:47:52 -0300

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,13 @@ dependencies {
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web:4.0.0")
-
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:4.0.0")
-
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
     testImplementation("org.springframework.boot:spring-boot-starter-test:4.0.0")
+
+    // https://mvnrepository.com/artifact/com.h2database/h2
+    implementation("com.h2database:h2:2.4.240")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
